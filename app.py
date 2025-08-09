b71900a0edfc836b66d6c6ec1d20d3bb4787a768
Diego09cr <diego.criscuolo09@hotmail.com>
2025-08-09 13:19:38 -0300

    feat: add POST route to create new record

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
+users = {}
+users_id = 1
+
+@app.route('/User', methods=['POST'])
+def createuser():
+    global users_id
+    data_json = request.get_json()
+    user = {
+        "id": users_id,
+        "name": data_json.get("name"),
+        "email": data_json.get("email")
+    }
+    users[users_id] = user
+    users_id += 1
+    return jsonify(user)
 
 if __name__ == '__main__':
     app.run(debug=True)
